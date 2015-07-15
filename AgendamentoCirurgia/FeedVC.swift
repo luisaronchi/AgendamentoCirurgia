b1a0228c35beb47ef736abb8a5ed99e53e02906a
alenamiklos <alenamiklos@Alenas-MacBook-Pro.local>
2015-07-15 16:23:47 -0300

    View & Controller (outlet and actions)

--- AgendamentoCirurgia/FeedVC.swift
+++ AgendamentoCirurgia/FeedVC.swift
@@ -17,12 +17,10 @@ class FeedVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
--- AgendamentoCirurgia/RequestVC.swift
+++ AgendamentoCirurgia/RequestVC.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class RequestVC: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var birthField: UITextField!
 
+    @IBOutlet weak var insuranceField: UITextField!
+    
+    @IBOutlet weak var nameDoctorField: UITextField!
+    
+    @IBOutlet weak var phoneField: UITextField!
+
+    @IBOutlet weak var surgeryTypeField: UITextField!
+    
+    @IBOutlet weak var surgeryDateField: UITextField!
+    
+    @IBOutlet weak var materialField: UITextField!
+    
+    @IBOutlet weak var observationField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
