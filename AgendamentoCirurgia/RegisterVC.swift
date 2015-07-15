a7ed22ad1c2848f5c6830ceead7020ae790f138c
alenamiklos <alenamiklos@Alenas-MacBook-Pro.local>
2015-07-15 14:14:55 -0300

    Ajustando view e controller do login e register

--- AgendamentoCirurgia/RegisterVC.swift
+++ AgendamentoCirurgia/RegisterVC.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class RegisterVC: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var crmField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var phoneField: UITextField!
+    
+    @IBOutlet weak var singinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +32,6 @@ class RegisterVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
