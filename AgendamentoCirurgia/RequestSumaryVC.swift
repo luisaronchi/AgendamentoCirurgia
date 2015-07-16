8755b70fee983e7ad91f49249e25e94577cf8b44
alenamiklos <alenamiklos@Alenas-MacBook-Pro.local>
2015-07-16 16:51:34 -0300

    view & controller do request summary

--- AgendamentoCirurgia/RequestSumaryVC.swift
+++ AgendamentoCirurgia/RequestSumaryVC.swift
@@ -9,27 +9,33 @@
 import UIKit
 
 class RequestSumaryVC: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
 
+    @IBOutlet weak var birthLabel: UILabel!
+
+    @IBOutlet weak var insuranceLabel: UILabel!
+    
+    @IBOutlet weak var nameDoctorLabel: UILabel!
+    
+    @IBOutlet weak var phoneLabel: UILabel!
+    
+    @IBOutlet weak var typeSurgeryLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var materialLabel: UILabel!
+    
+    @IBOutlet weak var observationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
