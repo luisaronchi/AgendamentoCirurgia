3e0f11805ef6b5ff1c1d2fa1f39fdef9319adbd4
alenamiklos <alenamiklos@Alenas-MacBook-Pro.local>
2015-07-20 14:18:23 -0300

    Feed Funçoes

--- AgendamentoCirurgia/FeedVC.swift
+++ AgendamentoCirurgia/FeedVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBAction func NewRequest(sender: UIButton) {
         
@@ -19,14 +19,42 @@ class FeedVC: UIViewController {
     
     @IBOutlet weak var segment: UISegmentedControl!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return requestList.count
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // Request que eu devo usar para preencher a célula
+        let request = requestList[indexPath.row]
+        
+        let cellIdentifier = "requestCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+        
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIdentifier)
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        // TODO: Preencher informações
+        
+        cell.textLabel?.text = request.doctorName
+        
+        return cell
     }
-
-
 }
+
+
+// Gambiarra temporária
+var requestList: [Request] = [Request]()
+
+// (var ou let) variableName: VariableType
+
+// (var ou let) variableName(: VariableType) = VariableType()
+// Ex.:
+// let a: Int = 2   -> Jeito tradicional    (ERRADO)
+// let a = 2        -> Jeito Swift          (CERTO) BY RENEN
+
+// Array<Request> == [Request]
+// Array<Request>   -> Jeito tradicional    (ERRADO)
+// [Request]        -> Jeito Swift          (CERTO) BY RENEN
